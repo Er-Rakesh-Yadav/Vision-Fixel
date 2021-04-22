4884e2240c1af04f4f36ee8d239b4c7f2689069e
shivam <babaeminem37@gmail.com>
2021-04-22 21:36:53 +0530

    final edits

--- App/src/ImgEnhanceColor.py
+++ App/src/ImgEnhanceColor.py
@@ -16,7 +16,7 @@ class imgEnhanceColor():
         # size = width, height = image.size
 
         enhancer = ImageEnhance.Color(image)
-        # if we give 0.0 then it will inhance in white and black
+        # if we give 0.0 then it will enhance in white and black
         image = enhancer.enhance(0.5)
         toast("Operation Applied")
 
